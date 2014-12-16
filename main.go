f36096ce198b28f358531e6ca5e83972b2c2690f
Tyler Sommer <sommertm@gmail.com>
2014-12-16 11:19:05 -0800

    Added ability to daemonize squircy

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"github.com/tyler-sommer/squircy2/squircy"
+	"flag"
 )
+
+var daemonize bool
+func init() {
+	flag.BoolVar(&daemonize, "daemonize", true, "Run as a daemon")
+}
 
 func main() {
 	mgr := squircy.NewManager()
-	go mgr.Run()
 
-	mgr.LoopCli()
+	if (!daemonize) {
+		go mgr.Run()
+
+		mgr.LoopCli()
+
+	} else {
+		mgr.Run()
+	}
 }
